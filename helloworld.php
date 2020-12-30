8a286b768d2788e9a9cc0d3d6c56b81025f5a67f
ealpha072 <ealpha072@gmail.com>
2020-12-30 15:36:33 +0300

    Php if statement

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+  <?php 
+    $date = date('D');
+    if ($date == 'Fri') {
+      # code...
+      echo "Have a nice weekend";
+    }else {
+      echo "Have a anice day";
+    }
+
+  
+  ?>  
+</body>
+</html>
