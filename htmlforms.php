8a6f8f7c2a644f4aa2f03291706064a43414eef7
ealpha072 <ealpha072@gmail.com>
2020-12-30 18:14:54 +0300

    Get and post methods

--- htmlforms.php
+++ htmlforms.php
@@ -1,15 +1,21 @@
 <?php
-   if( $_POST["location"] ) {
+   /*if( $_POST["location"] ) {
       $location = $_POST["location"];
       header( "Location:$location" );
       
       exit();
-   }
+   }*/
+   if( $_GET["name"] || $_GET["age"] ) {
+    echo "Welcome ". $_GET['name']. "<br />";
+    echo "You are ". $_GET['age']. " years old.";
+    
+    exit();
 ?>
+
 <html>
    <body>
    
-      <p>Choose a site to visit :</p>
+      <!--<p>Choose a site to visit :</p>
       
       <form action = "<?php $_SERVER['PHP_SELF'] ?>" method ="POST">
          <select name = "location">.
@@ -23,6 +29,12 @@
             </option>
          
          </select>
+         <input type = "submit" />
+      </form>-->
+
+      <form action = "<?php $_PHP_SELF ?>" method = "GET">
+         Name: <input type = "text" name = "name" />
+         Age: <input type = "text" name = "age" />
          <input type = "submit" />
       </form>
       
