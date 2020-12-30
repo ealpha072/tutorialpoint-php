051957ff7bcefe05e2a3e690d317eb7a4dd8af12
ealpha072 <ealpha072@gmail.com>
2020-12-30 15:41:55 +0300

    For loop example

--- helloworld.php
+++ helloworld.php
@@ -7,8 +7,18 @@
       echo "Have a nice weekend";
     }else {
       echo "Have a anice day";
+      echo "<br>";
     }
 
+    $first = 0;
+    $second = 0;
+    
+    for ($i=0; $i <5 ; $i++) { 
+      # code...
+      $first +=5;
+      $second +=10;
+    }
+    echo 'At loop end, first is' .$first. ' while second is '.$second. '<br>';
   
   ?>  
 </body>
