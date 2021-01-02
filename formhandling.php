beb760c4d1e37703a8f432a70dc912e541406c40
ealpha072 <ealpha072@gmail.com>
2021-01-02 15:57:33 +0300

    Form now is submitted to the same php page

--- formhandling.php
+++ formhandling.php
@@ -1,7 +1,27 @@
+<?php 
+  $name = $email = $website = $comment= $gender= '' ;
+
+  if($_SERVER["REQUEST_METHOD"]=='POST'){
+    $name = sanitizeData($_POST['name']);
+    $email = sanitizeData($_POST['email']);
+    $website = sanitizeData($_POST['website']);
+    $comment = sanitizeData($_POST['comment']);
+    $gender = sanitizeData($_POST['gender']);
+  }
+
+
+  function sanitizeData($input){
+    $input = trim($input);
+    $input = stripcslashes($input);
+    $input = htmlspecialchars($input);
+    return $input;
+  }
+?>
+
 <html>
   <body>
-    <form action="welcome.php" method = 'post'>
-    <!--<form action="<?#php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">-->
+    <!--<form action="welcome.php" method = 'post'>-->
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
       <label for="name">Name</label><br>
       <input type="text" name='name' required><br>
       <label for="E-mail">E-mail</label><br>
@@ -17,6 +37,12 @@
       <input type="submit">
     </form>
     <h2>Your input: </h2>
-    
+    <ul>
+      <li>Name: <?php echo $name;?></li>
+      <li>E-mail: <?php echo $email;?></li>
+      <li>Website: <?php echo $website;?></li>
+      <li>Gender: <?php echo $gender;?></li>
+      <li>Comment: <?php echo $comment;?></li>
+  </ul>
   </body>
 </html>
