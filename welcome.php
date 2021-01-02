a883b96763134659bad0e665f2717a12bc50177e
ealpha072 <ealpha072@gmail.com>
2021-01-02 15:44:02 +0300

    Form verification

--- welcome.php
+++ welcome.php
@@ -1,6 +1,34 @@
+<?php 
+  $name = $email = $website = $comment= $gender= '' ;
+
+  if($_SERVER["REQUEST_METHOD"]=='POST'){
+    $name = sanitizeData($_POST['name']);
+    $email = sanitizeData($_POST['email']);
+    $website = sanitizeData($_POST['website']);
+    $comment = sanitizeData($_POST['comment']);
+    $gender = sanitizeData($_POST['gender']);
+  }
+
+
+  function sanitizeData($input){
+    $input = trim($input);
+    $input = stripcslashes($input);
+    $input = htmlspecialchars($input);
+    return $input;
+  }
+?>
+
+
+
 <html>
 <body>
-  <p>Welcome <?php echo $_POST['name'];?></p> 
-  <p>Your email address is <?php echo $_POST['email'];?></p> 
+  <h2>Your info is as follows:</h2> 
+  <ul>
+    <li>Name: <?php echo $name;?></li>
+    <li>E-mail: <?php echo $email;?></li>
+    <li>Website: <?php echo $website;?></li>
+    <li>Gender: <?php echo $gender;?></li>
+    <li>Comment: <?php echo $comment;?></li>
+  </ul>
 </body>
 </html>
