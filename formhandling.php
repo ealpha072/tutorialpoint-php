fb407377afa27e407e3efffd5c72b9a769d3497d
ealpha072 <ealpha072@gmail.com>
2021-01-02 15:21:32 +0300

    Form and validation files

---
+++ formhandling.php
@@ -0,0 +1,11 @@
+<html>
+  <body>
+    <form action="welcome.php" method = 'post'>
+      <label for="name">Name</label>
+      <input type="text" name='name'>
+      <label for="E-mail">E-mail</label>
+      <input type="email" name='email'>
+      <input type="submit">
+    </form>
+  </body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+  <p>Welcome <?php echo $_POST['name'];?></p> 
+  <p>Your email address is <?php echo $_POST['email'];?></p> 
+</body>
+</html>
